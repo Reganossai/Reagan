f8cbadce92e46dc3d45eca530ae4a5050380bc28
Ossai Reagan <ossaireagano@gmail.com>
2023-06-24 17:52:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
